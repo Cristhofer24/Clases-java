2c445b297c7ff6944eccbe73ccb71bfc986fe882
cristhofer24 <you@example.com>
2024-09-26 20:37:25 -0500

    Clases en java

---
+++ src/Cuenta.java
@@ -0,0 +1,10 @@
+//  Entidad cuenta tiene cuatro atribotos;
+//saldo , agencia , numero , y titular
+
+public class Cuenta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+
+}
